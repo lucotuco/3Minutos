8049066faa0b247745d013c99d7ca4943112ebce
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-04-08 16:15:03 -0300

    cors instalado

--- 3MinutosBack/src/app.js
+++ 3MinutosBack/src/app.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const cors = require('cors');
 const articleRoutes = require('./routes/articleRoutes');
-const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get('/health', (req, res) => {
-  res.json({ ok: true });
-});
-
 app.get('/', (req, res) => {
-  res.json({ ok: true, service: '3Minutos backend' });
+  res.send('API de 3 Minutos funcionando');
 });
 
 app.use('/articles', articleRoutes);
-app.use('/users', userRoutes);
 
 module.exports = app;
